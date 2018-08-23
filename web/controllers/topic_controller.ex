e7afd377ca9ac27d5c15f0d2001fc7241c564a70
ANDREW SEGAL <segal.a@hotmail.com>
2018-08-23 09:02:54 -0400

    Add users to create new topics

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,26 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def index(conn, _params) do
+
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, topic} ->
+        Repo.insert(topic)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+  end
+end
