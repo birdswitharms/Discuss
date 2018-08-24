ee99ee579679d54ddab9bcea4454d21ddfb9caf5
ANDREW SEGAL <segal.a@hotmail.com>
2018-08-23 22:42:16 -0400

    Add Update feature to Topics

--- web/router.ex
+++ web/router.ex
@@ -19,7 +19,8 @@ defmodule Discuss.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics/new", TopicController, :create
-
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
   end
 
   # Other scopes may use custom stacks.
