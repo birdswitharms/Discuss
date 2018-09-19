76f897ab9e952434af3611a3d1b6c0813b6fd9ea
ANDREW SEGAL <segal.a@hotmail.com>
2018-09-19 09:26:04 -0400

    Fetch correct topic in the comments_channel

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,11 +1,15 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
+  alias Discuss.Topic
 
-  def join(name, _params, socket) do
-    {:ok, %{hello: "world"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, socket}
   end
 
-  def handle_in() do
-
+  def handle_in(name, message, socket) do
+    {:reply, :ok, socket}
   end
 end
